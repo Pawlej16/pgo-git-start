1e3d8160ed408405053958a7fee0fb45bdeed505
Pawlej16 <pawel.sztenke@gmail.com>
2026-03-13 10:45:07 +0100

    Method added to the Main.java file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HElLO WORLD!!!");
+
+        fourthCommit;
+    }
+
+    public static void fourthCommit(Sring[] args) {
+        System.out.print("These are changes added in 4th commit");
     }
 }
